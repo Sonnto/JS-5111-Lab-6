df0bad382bc8f8f86e4fff0e823102dc28895280
Sonnto <90372802+Sonnto@users.noreply.github.com>
2022-11-01 18:12:46 -0400

    JS lab 6 on Document Object Model (DOM)

---
+++ lab6-2.js
@@ -0,0 +1,32 @@
+﻿//#### LAB 6 - DOM MANIPULATION ####
+//PART 2: MYSTERY BOX
+// alert("test");//COMMENT OUT AS SOON AS YOU KNOW YOU ARE CONNECTED!!!!
+
+//LISTEN FOR load EVENT
+window.onload = pageReady;
+
+//'WRAPPER' FUNCTION FOR DOM LOGIC
+function pageReady() {
+  //GET DOM ELEMENTS WE'LL NEED
+  let theBox = document.getElementById("mysteryBox");
+  let theNewBox = document.getElementById("buttonBox");
+
+  //====CREATE THE FUNCTIONS WE'LL NEED====
+  //FUNCTION TO ASK USER
+  function potentialSurprise() {
+    let surprise = confirm("Do you want to see something?");
+    if (surprise === true) {
+      mysteryBox.style.display = "none";
+      buttonBox.style.display = "block";
+    }
+  }
+  //FUNCTION TO CHANGE buttonBox
+  function transform() {
+    theNewBox.style.width = "600px";
+    theNewBox.innerHTML = "<h2>SURPRISE!!</h2>";
+    theNewBox.style.backgroundColor = "orange";
+  }
+  //SETUP LISTENERS
+  theBox.onmouseover = potentialSurprise;
+  theNewBox.onclick = transform;
+} //END onload FUNCTION
